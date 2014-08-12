f56d4719fd7c6b2ca03ff3828ae89dc42cdedf5e
jos <wjosdejong@gmail.com>
2014-08-12 16:28:15 +0200

    Added an example demonstrating how to create a custom Actor prototype

---
+++ examples/prototype_extension.js
@@ -0,0 +1,53 @@
+// This example shows how to extend an Actor
+var actors = require('../index');
+
+/**
+ * Custom actor prototype
+ * @param {String} id
+ * @constructor
+ * @extend actors.Actor
+ */
+function MyActor(id) {
+  // execute super constructor function
+  actors.Actor.call(this, id);
+
+  // listen for greetings
+  this.on(/hi|hello/i, this.onGreeting.bind(this));
+}
+
+// extend the actors.Actor prototype
+MyActor.prototype = Object.create(actors.Actor.prototype);
+MyActor.prototype.constructor = MyActor;
+
+/**
+ * Send a greeting to an actor
+ * @param {String} to
+ */
+MyActor.prototype.sayHi = function (to) {
+  this.send(to, 'Hi!');
+};
+
+/**
+ * Handle incoming greetings
+ * @param {String} from
+ * @param {String} message
+ */
+MyActor.prototype.onGreeting = function (from, message) {
+  console.log(from + ' said: ' + message);
+};
+
+var bus = new actors.LocalMessageBus();
+var actor1 = new MyActor('actor1');
+var actor2 = new MyActor('actor2');
+
+// connect both actors to the message bus
+actor1.connect(bus);
+actor2.connect(bus);
+
+// send a message to actor 1
+actor2.sayHi('actor1');
+
+// Test prototype inheritance:
+// console.log(actor1 instanceof MyActor);       // true
+// console.log(actor1 instanceof actors.Actor);  // true
+// console.log(actor1.constructor.name);         // 'MyActor'
