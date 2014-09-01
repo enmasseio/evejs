bd7c039f8bb6c64c6e71f8b61f166753e91ff82f
jos <wjosdejong@gmail.com>
2014-09-01 17:02:46 +0200

    Replaced double quotes with single quotes

--- examples/httpTransport.js
+++ examples/httpTransport.js
@@ -1,29 +1,24 @@
 var Promise = require('promise');
 var eve = require('../index');
+var HelloAgent = require('./agents/HelloAgent');
 
 eve.system.init({
   transports: [
     {
       type: 'http',
-      port:3000,
-      url:"http://127.0.0.1:3000/agents/:id",
-      remoteUrl: "http://127.0.0.1:3000/agents/:id",
-      localShortcut:false,
-      default:true
+      port: 3000,
+      url: 'http://127.0.0.1:3000/agents/:id',
+      remoteUrl: 'http://127.0.0.1:3000/agents/:id',
+      localShortcut: false,
+      'default': true
     }
   ]
 });
 
-// agent 1 listens for messages containing 'hi' or 'hello' (case insensitive)
-var agent1 = new eve.Agent('agent1');
-var agent2 = new eve.Agent('agent2');
-agent1.receive = function (from, message) {
-  console.log(from + ' said: ' + message);
-};
+// create two agents
+var agent1 = new HelloAgent('agent1');
+var agent2 = new HelloAgent('agent2');
 
-// Connect to all configured transports
-agent1.connect(eve.system.transports.get());
-agent2.connect(eve.system.transports.get());
-
-// send a message to agent 1
-agent2.send('agent1', 'Hello agent1!');
+// send a message to agent1
+//agent2.send('agent1', 'Good morning');
+agent2.send('http://127.0.0.1:3000/agents/agent1', 'Good morning');
