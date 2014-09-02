b123cd2491ff294073d12ed9e3dbb2d045c608eb
jos <wjosdejong@gmail.com>
2014-09-02 09:25:51 +0200

    Implemented support for sending to a url via HTTPTransport

--- examples/httpTransport.js
+++ examples/httpTransport.js
@@ -8,7 +8,7 @@ eve.system.init({
       type: 'http',
       port: 3000,
       url: 'http://127.0.0.1:3000/agents/:id',
-      remoteUrl: 'http://127.0.0.1:3000/agents/:id',
+      // remoteUrl: 'http://127.0.0.1:3000/agents/:id',
       localShortcut: false,
       'default': true
     }
@@ -20,6 +20,4 @@ var agent1 = new HelloAgent('agent1');
 var agent2 = new HelloAgent('agent2');
 
 // send a message to agent1
-// agent2.sayHello('agent1');
-agent2.send('agent1', 'Good morning');
-//agent2.send('http://127.0.0.1:3000/agents/agent1', 'Good morning'); // TODO: not yet supported
+agent2.sayHello('http://127.0.0.1:3000/agents/agent1');
