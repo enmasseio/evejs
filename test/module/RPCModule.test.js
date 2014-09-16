c41d9fa337a5fe2e20c902f3f9e891317ec4e2cf
jos <wjosdejong@gmail.com>
2014-09-16 17:02:33 +0200

    Refactored RPCModule tests

--- test/module/RPCModule.test.js
+++ test/module/RPCModule.test.js
@@ -3,10 +3,12 @@ var LocalTransport = require('../../lib/transport/local/LocalTransport');
 var Agent = require('../../lib/Agent');
 
 describe ('RPC', function () {
+  var agent1;
+  var agent2;
 
-  it('should load an RPC module', function () {
-    var agent1 = new Agent('agent1');
-    var agent2 = new Agent('agent2');
+  before(function () {
+    agent1 = new Agent('agent1');
+    agent2 = new Agent('agent2');
 
     var transport = new LocalTransport();
     agent1.connect(transport);
@@ -15,6 +17,7 @@ describe ('RPC', function () {
     var sender = 'agent2';
     var checkParams = {a:1,b:3};
 
+
     agent1.rpcFunctions = {};
     agent2.rpcFunctions = {};
     agent1.rpcFunctions.add = function (params, from) {
@@ -22,113 +25,68 @@ describe ('RPC', function () {
       assert.deepEqual(params, checkParams);
       return params.a + params.b;
     };
-    agent1.rpc = agent1.loadModule("rpc", agent1.rpcFunctions);
-    agent2.rpc = agent2.loadModule("rpc", agent2.rpcFunctions);
+    agent1.rpcFunctions.nothing = function () {}; // does not return anything
+    agent1.extend("rpc", agent1.rpcFunctions);
+    agent2.extend("rpc", agent2.rpcFunctions);
+  });
 
-    return agent2.rpc.request("agent1",{method:"add",params:{a:1,b:3}}).then(
+  it('should load an RPC module', function () {
+    var agent3 = new Agent('agent3');
+    var agent4 = new Agent('agent4');
+
+    var transport = new LocalTransport();
+    agent3.connect(transport);
+    agent4.connect(transport);
+
+    var sender = 'agent4';
+    var checkParams = {a:1,b:3};
+
+    agent3.rpcFunctions = {};
+    agent4.rpcFunctions = {};
+    agent3.rpcFunctions.add = function (params, from) {
+      assert.equal(from, sender);
+      assert.deepEqual(params, checkParams);
+      return params.a + params.b;
+    };
+    agent3.rpc = agent3.loadModule("rpc", agent3.rpcFunctions);
+    agent4.rpc = agent4.loadModule("rpc", agent4.rpcFunctions);
+
+    return agent4.rpc.request("agent3",{method:"add",params:{a:1,b:3}}).then(
       function (reply) {
         assert.equal(reply, 4);
       })
   });
 
   it('should extend the agent with an RPC module', function () {
-    var agent1 = new Agent('agent1');
-    var agent2 = new Agent('agent2');
-
-    var transport = new LocalTransport();
-    agent1.connect(transport);
-    agent2.connect(transport);
-
-    var sender = 'agent2';
-    var checkParams = {a:1,b:3};
-
-
-    agent1.rpcFunctions = {};
-    agent2.rpcFunctions = {};
-    agent1.rpcFunctions.add = function (params, from) {
-      assert.equal(from, sender);
-      assert.deepEqual(params, checkParams);
-      return params.a + params.b;
-    };
-    agent1.rpc = agent1.extend("rpc", agent1.rpcFunctions);
-    agent2.rpc = agent2.extend("rpc", agent2.rpcFunctions);
-
-    return agent2.request("agent1",{method:"add",params:{a:1,b:3}}).then(
-      function (reply) {
-        assert.equal(reply, 4);
-      })
+    return agent2.request("agent1",{method:"add",params:{a:1,b:3}})
+        .then(function (reply) {
+          assert.equal(reply, 4);
+        })
   });
 
   it('should catch not having a method', function () {
-    var agent1 = new Agent('agent1');
-    var agent2 = new Agent('agent2');
-
-    var transport = new LocalTransport();
-    agent1.connect(transport);
-    agent2.connect(transport);
-
-    agent1.rpcFunctions = {};
-    agent2.rpcFunctions = {};
-    agent1.rpc = agent1.extend("rpc", agent1.rpcFunctions);
-    agent2.rpc = agent2.extend("rpc", agent2.rpcFunctions);
-
     return agent2.request("agent1",{params:{a:1,b:3}})
-      .then()
-      .catch(function (err) {assert.equal(err, "Error: Property \"method\" expected")})
+        .catch(function (err) {
+          assert.equal(err, "Error: Property \"method\" expected")
+        })
   });
 
   it('should catch method not found', function () {
-    var agent1 = new Agent('agent1');
-    var agent2 = new Agent('agent2');
-
-    var transport = new LocalTransport();
-    agent1.connect(transport);
-    agent2.connect(transport);
-
-    agent1.rpcFunctions = {};
-    agent2.rpcFunctions = {};
-    agent1.rpc = agent1.extend("rpc", agent1.rpcFunctions);
-    agent2.rpc = agent2.extend("rpc", agent2.rpcFunctions);
-
-    return agent2.request("agent1",{method:"add",params:{a:1,b:3}})
-      .then()
-      .catch(function (err) {assert.equal(err, "Error: Cannot find function: add")})
+    return agent2.request("agent1",{method:"foo",params:{a:1,b:3}})
+        .catch(function (err) {
+          assert.equal(err, "Error: Cannot find function: foo")
+        })
   });
 
   it('should catch message not an object', function () {
-    var agent1 = new Agent('agent1');
-    var agent2 = new Agent('agent2');
-
-    var transport = new LocalTransport();
-    agent1.connect(transport);
-    agent2.connect(transport);
-
-    agent1.rpcFunctions = {};
-    agent2.rpcFunctions = {};
-    agent1.rpc = agent1.extend("rpc", agent1.rpcFunctions);
-    agent2.rpc = agent2.extend("rpc", agent2.rpcFunctions);
-
     return agent2.request("agent1","hello")
-      .then()
-      .catch(function (err) {assert.equal(err.toString(), "TypeError: Message must be an object")})
+        .catch(function (err) {
+          assert.equal(err.toString(), "TypeError: Message must be an object")
+        })
   });
 
   it('should catch undefined return value', function () {
-    var agent1 = new Agent('agent1');
-    var agent2 = new Agent('agent2');
-
-    var transport = new LocalTransport();
-    agent1.connect(transport);
-    agent2.connect(transport);
-
-    agent1.rpcFunctions = {
-      foo: function () {}
-    };
-    agent2.rpcFunctions = {};
-    agent1.rpc = agent1.extend("rpc", agent1.rpcFunctions);
-    agent2.rpc = agent2.extend("rpc", agent2.rpcFunctions);
-
-    return agent2.request("agent1", {method: "foo"})
+    return agent2.request("agent1", {method: "nothing"})
       .then(function (reply) {
         assert.strictEqual(reply, null)
       })
@@ -139,21 +97,10 @@ describe ('RPC', function () {
   });
 
   it('should catch no message', function () {
-    var agent1 = new Agent('agent1');
-    var agent2 = new Agent('agent2');
-
-    var transport = new LocalTransport();
-    agent1.connect(transport);
-    agent2.connect(transport);
-
-    agent1.rpcFunctions = {};
-    agent2.rpcFunctions = {};
-    agent1.rpc = agent1.extend("rpc", agent1.rpcFunctions);
-    agent2.rpc = agent2.extend("rpc", agent2.rpcFunctions);
-
     return agent2.request("agent1")
-      .then()
-      .catch(function (err) {assert.equal(err.toString(), "TypeError: Message must be an object")})
+        .catch(function (err) {
+          assert.equal(err.toString(), "TypeError: Message must be an object")
+        })
   });
 
   // TODO: test whether the response has the same id as the request.
