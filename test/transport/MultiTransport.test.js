74211491610d9f6298d9f016ba0ac80a24eb1278
Alex de Mulder <alexdemulder@gmail.com>
2015-03-05 12:38:59 +0100

    hacked transport http and ws to work on the same server. in need of cleaner solution.

--- test/transport/MultiTransport.test.js
+++ test/transport/MultiTransport.test.js
@@ -3,7 +3,7 @@ var HTTPTransport = require('../../lib/transport/http/HTTPTransport');
 var WebSocketTransport = require('../../lib/transport/websocket/WebSocketTransport');
 var Promise = require('promise');
 var Agent = require('../../lib/Agent');
-describe('HTTPTransport', function() {
+describe('Multitransport on the same port', function() {
   var agent1;
   var agent2;
   var wsTransport;
@@ -13,40 +13,43 @@ describe('HTTPTransport', function() {
     agent1 = new Agent('agent1');
     agent2 = new Agent('agent2');
 
-    wsTransport = new WebSocketTransport({
-      type: 'ws',
-      url: 'ws://127.0.0.1:' + 3000 + '/agents/:id',
+    httpTransport = new HTTPTransport({
+      type: 'http',
+      url: 'http://127.0.0.1:' + 3000 + '/agents/:id',
       localShortcut: false
     });
-    httpTransport = new HTTPTransport({
-        type: 'http',
-        url: 'http://127.0.0.1:' + 3000 + '/agents/:id',
-        localShortcut: false
-    });
-
 
     agent1.connect(httpTransport);
-    agent1.connect(wsTransport);
-    agent2.connect(httpTransport);
-    agent2.connect(wsTransport);
-
-    agent2.receive = function (from, message) {
-      console.log(from, message);
-      if (message == 'reply to me!') {
-        this.send(from, 'hello back!');
-      }
-    }
-
-    agent1.receive = function(from, message) {
-      assert.equal(message, 'hello back!');
-      assert.equal(from, 'http://127.0.0.1:3000/agents/agent2');
-    }
-
-    return Promise.all([wsTransport.ready, httpTransport.ready]).then(function(a) {console.log(a)}).done();
+    return agent1.ready.then(function () {
+      wsTransport = new WebSocketTransport({
+        type: 'ws',
+        url: 'ws://127.0.0.1:' + 3000 + '/agents/:id',
+        localShortcut: false,
+        httpTransport:httpTransport
+      });
+      agent1.connect(wsTransport);
+      return agent1.ready;
+    }).then(function() {
+      agent2.connect(httpTransport);
+      agent2.connect(wsTransport);
+    }).done();
   });
 
   it('Should be able to use websockets and HTTP on the same port', function () {
-    //agent1.send('http://127.0.0.1:3000/agents/agent2','reply to me!').done();
-    //agent1.send('ws://127.0.0.1:3000/agents/agent2','reply to me!').done();
+    return new Promise( function(resolve, reject) {
+      var counter = 0;
+
+      agent2.receive = function (from, message) {
+        assert.equal(message, 'reply to me!');
+        counter++;
+        console.log("received from:", from, counter);
+        if (counter == 2) {
+          resolve();
+        }
+      }
+
+      agent1.send('http://127.0.0.1:3000/agents/agent2', 'reply to me!');
+      agent1.send('ws://127.0.0.1:3000/agents/agent2', 'reply to me!');
+    });
   });
 });
