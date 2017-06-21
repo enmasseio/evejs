76c7cc13a48b14c1a6cc4409fa76e63cf71850b4
Ludo Stellingwerff <ludo@almende.org>
2017-06-21 15:55:18 +0200

    Update configuration possibilities for DBus transport.

--- examples/transports/dbus.js
+++ examples/transports/dbus.js
@@ -5,8 +5,7 @@ eve.system.init({
   transports: [
     {
       type: 'dbus',
-      url: 'dbus:com.almende.eve/com/almende/eve/agent/:id',
-      remoteUrl: 'dbus:com.almende.eve/com/almende/eve/agent/:id',
+      url: 'dbus:com.almende.eve/agent/:id',
       localShortcut: false,
       default: true
     }
@@ -17,19 +16,20 @@ eve.system.init({
 var agent1 = new HelloAgent('agent1');
 var agent2 = new HelloAgent('agent2');
 
-setTimeout(function () {
-
+Promise.all([agent1.ready, agent2.ready]).then(function () {
   // send a message to agent1
-  agent1.sayHello('dbus:com.almende.eve/com/almende/eve/agent/agent2');
+  agent1.sayHello('dbus:com.almende.eve/agent/agent2');
 
-// alternative:
+  // alternative:
   agent2.sayHello('agent1'); // this works because of the remoteUrl of the transport.
 
-// catch error
-//    agent2.send("agent3", "Hello").catch(function (err) {
-//      console.log(err)
-//    });
+  // catch error
+  //   agent2.send("agent3", "Hello").catch(function (err) {
+  //     console.log(err)
+  // });
+}, function(){
+  console.log("Agents rejected getting ready.");
+});
 
-}, 500);
 
 
