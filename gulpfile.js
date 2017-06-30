f1cecd696bc6f77387b227b5c7f0e423b90d12b7
Ludo Stellingwerff <ludo@almende.org>
2017-06-30 15:09:59 +0200

    Reimplemented the entire bundle/babel/minify, because of modules being updated to es6 syntax and other issues.

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,103 @@
-var fs = require('fs');
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var webpack = require('webpack');
-var uglify = require('uglify-es');
-var path = require('path');
 
-var ENTRY       = './index.js';
-var FILE        = 'eve.js';
-var FILE_MIN    = 'eve.min.js';
-var FILE_MAP    = 'eve.map';
-var DIST        = path.resolve(__dirname, 'dist');
-var EVE_JS      = DIST + '/' + FILE;
-var EVE_MIN_JS  = DIST + '/' + FILE_MIN;
-var EVE_MAP_JS  = DIST + '/' + FILE_MAP;
+var gutil = require('gulp-util');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var livereload = require('gulp-livereload');
+var clean = require('gulp-clean');
+var webpackstream = require('webpack-stream');
+var webpack = require('webpack');
+var rename = require('gulp-rename');
+
+var path = require('path');
+var fs = require('fs');
+
+var ENTRY = './index.js';
+var FILE = 'eve.js';
+var FILE_MIN = 'eve.min.js';
+var FILE_MAP = 'eve.map';
+var DIST = path.resolve(__dirname, 'dist');
+var EVE_JS = DIST + '/' + FILE;
+var EVE_MIN_JS = DIST + '/' + FILE_MIN;
+var EVE_MAP_JS = DIST + '/' + FILE_MAP;
 
 var webpackConfig = {
-  entry: ENTRY,
+  node: {
+    fs: "empty",
+    tls: "empty",
+    child_process: "empty"
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/amqplib/)
+  ],
+  module: {
+    exprContextCritical: false,
+    loaders: [
+      {test: /\.json/, loader: 'json-loader'},
+      {test: /\.md/, loader: 'ignore-loader'}
+    ]
+  },
+  cache: true,
   output: {
     library: 'eve',
     libraryTarget: 'umd',
     path: DIST,
     filename: FILE
-  },
-
-  // TODO: exclude all non-relevant libraries from the browser bundle
-  node: {
-    fs: "empty",
-    tls: "empty"
-  },
-  plugins: [
-    new webpack.IgnorePlugin(/amqplib/)
-  ],
-
-  cache: true
+  }
 };
 
 var uglifyConfig = {
-  outSourceMap: FILE_MAP,
+//  outSourceMap: FILE_MAP,
+
+  mangle: true,
+  compress: {
+    sequences: true,
+    dead_code: true,
+    conditionals: true,
+    booleans: true,
+    unused: true,
+    if_return: true,
+    join_vars: true,
+    drop_console: true
+  },
   output: {
     comments: /@license/
   }
 };
+gulp.task('build', function () {
+  // app.js is your main JS file with all your module inclusions
 
-// create a single instance of the compiler to allow caching
-
-var compiler = webpack(webpackConfig);
-gulp.task('bundle', function (cb) {
-  // TODO: add a header (banner) on top of the file
-  compiler.run(function (err, stats) {
-    if (err) {
-      gutil.log(err);
-    }
-
-    gutil.log('bundled ' + EVE_JS);
-
-    cb();
-  });
+  return gulp.src(ENTRY)
+    .pipe(webpackstream(webpackConfig))
+    .pipe(babel({
+      presets: ['es2015'],
+      sourceType: "script",
+      compact: true
+    }))
+    .pipe(gulp.dest(DIST))
+    .pipe(livereload())
 });
 
-gulp.task('minify', ['bundle'], function () {
-  var result = uglify.minify([EVE_JS], uglifyConfig);
-
-  fs.writeFileSync(EVE_MIN_JS, result.code);
-  fs.writeFileSync(EVE_MAP_JS, result.map);
-
-  gutil.log('Minified ' + EVE_MIN_JS);
-  gutil.log('Mapped ' + EVE_MAP_JS);
+gulp.task('compress', ['build'], function () {
+  return gulp.src(EVE_JS)
+    .pipe(rename(FILE_MIN))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify(uglifyConfig).on('error', function (err) {
+      gutil.log(gutil.colors.red('[Error]'), err.toString());
+      this.emit('end');
+    }))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(DIST))
 });
 
-// The default task (called when you run `gulp`)
-gulp.task('default', ['bundle', 'minify']);
-
-// Watch task to automatically bundle and minify on change of code
-gulp.task('watch', ['bundle', 'minify'], function () {
-  gulp.watch(['index.js', 'lib/**/*.js'], ['bundle', 'minify']);
+gulp.task('watch', ['build'], function () {
+  livereload.listen();
+  gulp.watch(['index.js', 'lib/**/*.js'], ['build']);
 });
 
-// Watch task to automatically bundle on change of code
-gulp.task('watch-dev', ['bundle'], function () {
-  gulp.watch(['index.js', 'lib/**/*.js'], ['bundle']);
-});
+gulp.task('default', ['watch']);
